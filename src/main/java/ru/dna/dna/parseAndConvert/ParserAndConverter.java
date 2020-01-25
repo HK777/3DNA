a7cde42d7a0dff78ef16f5b11b079d4637c2ce6c
HK777 <aykaltunyan10@mail.ru>
2020-01-25 20:58:39 +0300

    Add ParserAndConverter

--- src/main/java/ru/dna/dna/parseAndConvert/ParserAndConverter.java
+++ src/main/java/ru/dna/dna/parseAndConvert/ParserAndConverter.java
@@ -74,50 +74,6 @@ public class ParserAndConverter {
     private String convertToStringXML(String dnaName, ArrayList<String[]> cells) {
         StringBuilder convertedToStringXml = new StringBuilder();
 
-        String stringXMLBeginTemplate = "<?xml version=\"1.0\"?>\n" +
-                "<?mso-application progid=\"Excel.Sheet\"?>\n" +
-                "<Workbook xmlns=\"urn:schemas-microsoft-com:office:spreadsheet\"\n" +
-                " xmlns:o=\"urn:schemas-microsoft-com:office:office\"\n" +
-                " xmlns:x=\"urn:schemas-microsoft-com:office:excel\"\n" +
-                " xmlns:ss=\"urn:schemas-microsoft-com:office:spreadsheet\"\n" +
-                " xmlns:html=\"http://www.w3.org/TR/REC-html40\">\n" +
-                " <DocumentProperties xmlns=\"urn:schemas-microsoft-com:office:office\">\n" +
-                "  <LastAuthor>Microsoft Office User</LastAuthor>\n" +
-                "  <Created>2020-01-25T09:32:33Z</Created>\n" +
-                "  <Version>16.00</Version>\n" +
-                " </DocumentProperties>\n" +
-                " <OfficeDocumentSettings xmlns=\"urn:schemas-microsoft-com:office:office\">\n" +
-                "  <AllowPNG/>\n" +
-                " </OfficeDocumentSettings>\n" +
-                " <ExcelWorkbook xmlns=\"urn:schemas-microsoft-com:office:excel\">\n" +
-                "  <WindowHeight>16500</WindowHeight>\n" +
-                "  <WindowWidth>28240</WindowWidth>\n" +
-                "  <WindowTopX>280</WindowTopX>\n" +
-                "  <WindowTopY>460</WindowTopY>\n" +
-                "  <ProtectStructure>False</ProtectStructure>\n" +
-                "  <ProtectWindows>False</ProtectWindows>\n" +
-                " </ExcelWorkbook>\n" +
-                " <Styles>\n" +
-                "  <Style ss:ID=\"Default\" ss:Name=\"Normal\">\n" +
-                "   <Alignment ss:Vertical=\"Bottom\"/>\n" +
-                "   <Borders/>\n" +
-                "   <Font ss:FontName=\"Calibri\" x:CharSet=\"204\" x:Family=\"Swiss\" ss:Size=\"12\"\n" +
-                "    ss:Color=\"#000000\"/>\n" +
-                "   <Interior/>\n" +
-                "   <NumberFormat/>\n" +
-                "   <Protection/>\n" +
-                "  </Style>\n" +
-                "  <Style ss:ID=\"s62\">\n" +
-                "   <Font ss:FontName=\"Helvetica Neue\" x:Family=\"Swiss\" ss:Color=\"#000000\"\n" +
-                "    ss:Bold=\"1\"/>\n" +
-                "  </Style>\n" +
-                "  <Style ss:ID=\"s63\">\n" +
-                "   <Font ss:FontName=\"Helvetica Neue\" x:Family=\"Swiss\" ss:Color=\"#000000\"/>\n" +
-                "  </Style>\n" +
-                "  <Style ss:ID=\"s64\">\n" +
-                "   <Font ss:FontName=\"Helvetica\" x:Family=\"Swiss\" ss:Size=\"12\" ss:Color=\"#000000\"/>\n" +
-                "  </Style>\n" +
-                " </Styles>\n";
         int size = cells.size();
         int length = cells.get(0).length;
 
@@ -125,7 +81,7 @@ public class ParserAndConverter {
                 "  <Table ss:ExpandedColumnCount=\"" + length + 1 + "\" ss:ExpandedRowCount=\"" + size + "\" x:FullColumns=\"1\"\n" +
                 "   x:FullRows=\"1\" ss:DefaultColumnWidth=\"65\" ss:DefaultRowHeight=\"16\">\n";
 
-        convertedToStringXml.append(stringXMLBeginTemplate).append(stringXMLDnaBodyHead);
+        convertedToStringXml.append(stringXMLDnaBodyHead);
 
         convertedToStringXml.append("   <Row>\n");
 
