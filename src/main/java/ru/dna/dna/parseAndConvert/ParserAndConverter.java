a0965d74479c68e322c3abb5a7e8bc4ddbeff120
HK777 <aykaltunyan10@mail.ru>
2020-01-25 20:57:28 +0300

    Add ParserAndConverter

--- src/main/java/ru/dna/dna/parseAndConvert/ParserAndConverter.java
+++ src/main/java/ru/dna/dna/parseAndConvert/ParserAndConverter.java
@@ -3,6 +3,55 @@ package ru.dna.dna.parseAndConvert;
 import java.util.ArrayList;
 
 public class ParserAndConverter {
+    private String addXMLDnaHead(String convertedToStringXml) {
+        String stringXMLDnaHead = "<?xml version=\"1.0\"?>\n" +
+                "<?mso-application progid=\"Excel.Sheet\"?>\n" +
+                "<Workbook xmlns=\"urn:schemas-microsoft-com:office:spreadsheet\"\n" +
+                " xmlns:o=\"urn:schemas-microsoft-com:office:office\"\n" +
+                " xmlns:x=\"urn:schemas-microsoft-com:office:excel\"\n" +
+                " xmlns:ss=\"urn:schemas-microsoft-com:office:spreadsheet\"\n" +
+                " xmlns:html=\"http://www.w3.org/TR/REC-html40\">\n" +
+                " <DocumentProperties xmlns=\"urn:schemas-microsoft-com:office:office\">\n" +
+                "  <LastAuthor>Microsoft Office User</LastAuthor>\n" +
+                "  <Created>2020-01-25T09:32:33Z</Created>\n" +
+                "  <Version>16.00</Version>\n" +
+                " </DocumentProperties>\n" +
+                " <OfficeDocumentSettings xmlns=\"urn:schemas-microsoft-com:office:office\">\n" +
+                "  <AllowPNG/>\n" +
+                " </OfficeDocumentSettings>\n" +
+                " <ExcelWorkbook xmlns=\"urn:schemas-microsoft-com:office:excel\">\n" +
+                "  <WindowHeight>16500</WindowHeight>\n" +
+                "  <WindowWidth>28240</WindowWidth>\n" +
+                "  <WindowTopX>280</WindowTopX>\n" +
+                "  <WindowTopY>460</WindowTopY>\n" +
+                "  <ProtectStructure>False</ProtectStructure>\n" +
+                "  <ProtectWindows>False</ProtectWindows>\n" +
+                " </ExcelWorkbook>\n" +
+                " <Styles>\n" +
+                "  <Style ss:ID=\"Default\" ss:Name=\"Normal\">\n" +
+                "   <Alignment ss:Vertical=\"Bottom\"/>\n" +
+                "   <Borders/>\n" +
+                "   <Font ss:FontName=\"Calibri\" x:CharSet=\"204\" x:Family=\"Swiss\" ss:Size=\"12\"\n" +
+                "    ss:Color=\"#000000\"/>\n" +
+                "   <Interior/>\n" +
+                "   <NumberFormat/>\n" +
+                "   <Protection/>\n" +
+                "  </Style>\n" +
+                "  <Style ss:ID=\"s62\">\n" +
+                "   <Font ss:FontName=\"Helvetica Neue\" x:Family=\"Swiss\" ss:Color=\"#000000\"\n" +
+                "    ss:Bold=\"1\"/>\n" +
+                "  </Style>\n" +
+                "  <Style ss:ID=\"s63\">\n" +
+                "   <Font ss:FontName=\"Helvetica Neue\" x:Family=\"Swiss\" ss:Color=\"#000000\"/>\n" +
+                "  </Style>\n" +
+                "  <Style ss:ID=\"s64\">\n" +
+                "   <Font ss:FontName=\"Helvetica\" x:Family=\"Swiss\" ss:Size=\"12\" ss:Color=\"#000000\"/>\n" +
+                "  </Style>\n" +
+                " </Styles>\n";
+
+        return stringXMLDnaHead + convertedToStringXml;
+    }
+
     private String parseAndConvert(String dnaName, String stringToConvert) {
         String[] rows = stringToConvert.split("\n");
         rows[0] = replaceOrAddPairNum(rows[0]);
